34f7c7ef628ea3821155f803921054273bbabca8
kap4a149 <36609566+kap4a149@users.noreply.github.com>
2018-02-28 00:57:15 +0300

    Initial commit

---
+++ resources/views/mainPage.blade.php
@@ -0,0 +1,55 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <link href= {{ URL::asset('css/style.css') }} rel="stylesheet" type="text/css">
+    <title>Document</title>
+</head>
+<body>
+    <!-- Start og header -->
+    <header>
+    <div id="headerInfo">For information regarding the Lying in Honor of The Reverend Billy Graham, please visit our Media Center.</div>
+    <div id="logo">
+    <div id="underLogo"></div>
+    </div>
+    </header>
+    <!-- Header end -->
+
+    <!-- Start of menu -->
+    <div id="menu">
+        <div id="menuButtons">
+        <a href="#">Home</a>
+        <a href="#">The Department</a>
+        <a href="#">Careers</a>
+        <a href="#">Media Center</a>
+        <a href="#">Visiting Capitol Hill</a>
+        <a href="#">Contact Us</a>
+        </div>
+        <div id="textArea"><input type="text"></div>
+        <div id="buttonSearch"><button></button></div>
+    </div>
+    <!-- end of menu -->
+
+    <!-- Start of body -->
+    <div id="wrapper">
+asd
+
+    </div>
+    <!-- Body end -->
+    
+    <footer>
+    <div id="footerText">United States Capitol Police 119 D Street, NE Washington, DC 20510 (202) 224-1677
+        <div id="email">PIO@uscp.gov</div>
+    </div>
+    <div id="links">
+    <a href="#">Accesebility</a>
+    <a href="#">Copyring</a>
+    <a href="#">Privacy</a>
+    <a href="#">Tools</a>
+    </div>
+    </footer>
+
+</body>
+</html>
